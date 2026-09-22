5d988cc093017d239417b45b8414ceb321daefd6
sheerinbanun07 <sheerinbanun07@gmail.com>
2026-09-22 10:04:06 +0530

    Add Day 1 challenge

---
+++ challenge.py
@@ -0,0 +1,22 @@
+"""Challenge question."""
+
+from workflow import workflow
+from agent import agent
+
+QUESTION = (
+    "I can pay Rs. 30,000. "
+    "Which two courses can I take together "
+    "within this budget?"
+)
+
+print("Q:", QUESTION)
+
+print(
+    "\nWorkflow :",
+    workflow(QUESTION)
+)
+
+print(
+    "\nAgent :",
+    agent(QUESTION)
+)
